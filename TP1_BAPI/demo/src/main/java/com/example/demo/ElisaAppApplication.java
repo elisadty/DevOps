7aaa443053f84e7bc2e221abfe30814317d23b40
elisadty <116338465+elisadty@users.noreply.github.com>
2025-05-19 15:18:03 +0200

    commit 1

---
+++ TP1_BAPI/demo/src/main/java/com/example/demo/ElisaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElisaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElisaAppApplication.class, args);
+	}
+
+}
